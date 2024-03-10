19b44ba67f66b25ad4d8a6fb49ab901a3650cc71
Yurem <yuremtovar1@gmail.com>
2024-03-10 12:08:37 -0500

    Add sign-up layout component

---
+++ src/app/auth/sign-up/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"] 
+});
+
+export const metadata: Metadata = {
+  title: 'Task Tracker - Sign-Up',
+  description: 'Sign up for a new account.',
+};
+
+interface IRootLayout {
+  children: Readonly<React.ReactNode>
+}
+
+export default function RootLayout({ children }: IRootLayout) {
+  return (
+    <section>
+      {children}
+    </section>
+  );
+}
