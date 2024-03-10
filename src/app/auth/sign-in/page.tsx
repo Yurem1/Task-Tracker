4ae6d36fd17e3dd4c7e2bbd7f70b9ed01708dba4
Yurem <yuremtovar1@gmail.com>
2024-03-10 12:08:33 -0500

    Add sign-in page component

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home(): React.JSX.Element {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1>
+        hello
+      </h1>
+    </div>
+  );
+}
+
