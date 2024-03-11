e0aa227ce1ca1830b3c28dbd066bfedc71bf8188
Yurem <yuremtovar1@gmail.com>
2024-03-11 14:18:26 -0500

    Update grid rows in Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Page() {
   return (
-    <div className='grid grid-rows-2 items-start justify-items-center h-full'>
+    <div className='grid grid-rows-3 items-start justify-items-center h-full'>
       <div className='grid grid-rows-1'>
         <div className='border-b-2 border-black px-6 py-4'>
           <h1 className='text-center font-semibold'>
@@ -14,7 +14,7 @@ export default function Page() {
           </p>
         </div>
       </div>
-      <div className='grid grid-rows-2 h-40 w-64 bg-gray-200 py-4 px-4 rounded-xl text-center'>
+      <div className='self-center grid grid-rows-2 h-40 w-64 bg-gray-200 py-4 px-4 rounded-xl text-center'>
         <div className='grid grid-rows-2'>
           <div>
             <h1 className='font-semibold'>
