e9320bc2bc9b3bbafbbaacb4f218ccbbebaf0873
Yurem <yuremtovar1@gmail.com>
2024-03-10 23:10:36 -0500

    Rename Home component to Page

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Home(): React.JSX.Element {
+export default function Page(): React.JSX.Element {
   return (
     <div className='flex flex-col items-center justify-center'>
       <h1>
