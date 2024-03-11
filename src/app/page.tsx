c023c0e3df72af9064970653b0cfc29e19b760ff
Yurem <yuremtovar1@gmail.com>
2024-03-11 14:18:18 -0500

    Refactor page layout and add sign-in and sign-up links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,46 @@ import React from 'react';
 
 export default function Page() {
   return (
-    <div className='grid h-full'>
-      <div className='flex flex-col items-center border-b-2 border-black'>
-        <h1 className='font-semibold'>
-          Task Tracker
-        </h1>
-        <p className='font-medium'>
-          Create, update, and delete tasks.
-        </p>
-      </div>
-      <div className='grid size-2/5 bg-gray-200'>
-        <div>
-          <Link href='/auth/sign-in'>
-            <p>
-              Sign-In
-            </p>
-          </Link>
+    <div className='grid grid-rows-2 items-start justify-items-center h-full'>
+      <div className='grid grid-rows-1'>
+        <div className='border-b-2 border-black px-6 py-4'>
+          <h1 className='text-center font-semibold'>
+            Task Tracker
+          </h1>
+          <p className='text-center font-medium'>
+            Create, update, and delete tasks.
+          </p>
         </div>
       </div>
-      <div>
-        <h1>
-          hello
-        </h1>
+      <div className='grid grid-rows-2 h-40 w-64 bg-gray-200 py-4 px-4 rounded-xl text-center'>
+        <div className='grid grid-rows-2'>
+          <div>
+            <h1 className='font-semibold'>
+              Already have an account?
+            </h1>
+          </div>
+          <div>
+            <Link href='/auth/sign-in'>
+              <p className='font-medium'>
+                Sign-In
+              </p>
+            </Link>
+          </div>
+        </div>
+        <div className='grid grid-rows-2'>
+          <div>
+            <h1 className='font-semibold'>
+              Don't have an account?
+            </h1>
+          </div>
+          <div>
+            <Link href='/auth/sign-up'>
+              <p className='font-medium'>
+                Sign-Up
+              </p>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
