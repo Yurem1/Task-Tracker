be980b6f3457ba31e59eb235f33c5fcf7cf01583
Yurem <yuremtovar1@gmail.com>
2024-03-11 14:18:44 -0500

    Remove hello world heading

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function Page(): React.JSX.Element {
   return (
     <div className='flex flex-col items-center justify-center'>
-      <h1>
-        hello world
-      </h1>
+      
     </div>
   );
 }
