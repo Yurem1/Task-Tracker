0b9a710ac89396525ed2f6f72d150e0d22fd6025
Yurem <yuremtovar1@gmail.com>
2024-03-10 23:10:31 -0500

    Refactor page component layout and add sign-in link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
-export default function Home() {
+import Link from 'next/link';
+import React from 'react';
+
+export default function Page() {
   return (
-    <div className='flex flex-col'>
-      <h1>
-        Hello
-      </h1>
-      <h1>
-        Hello
-      </h1>
+    <div className='grid h-full'>
+      <div className='flex flex-col items-center border-b-2 border-black'>
+        <h1 className='font-semibold'>
+          Task Tracker
+        </h1>
+        <p className='font-medium'>
+          Create, update, and delete tasks.
+        </p>
+      </div>
+      <div className='grid size-2/5 bg-gray-200'>
+        <div>
+          <Link href='/auth/sign-in'>
+            <p>
+              Sign-In
+            </p>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <h1>
+          hello
+        </h1>
+      </div>
     </div>
   );
 }
