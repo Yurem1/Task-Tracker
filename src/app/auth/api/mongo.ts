d4290c4f13c25617e53e3853ffae5f9a5b43e809
Yurem <yuremtovar1@gmail.com>
2024-03-12 15:15:20 -0500

    add mongodb types, add utility interfaces, initialize db variables in constructor, add auth methods

--- src/app/auth/api/mongo.ts
+++ src/app/auth/api/mongo.ts
@@ -1,15 +1,61 @@
-import { MongoClient, Collection } from 'mongodb';
+import { ILogin, IProfile } from '@/utilities/interfaces';
+import { MongoClient, Collection, Db } from 'mongodb';
 
+/**
+ * @class
+ * Represents a MongoDB connection and provides methods to interact with the database.
+ */
 export class Mongo {
-  private mongoClient: MongoClient;
-  private collection: Collection;
+	private client: MongoClient;
+	private database: Db;
+	private collection: Collection;
 
-  public constructor() {
-    this.mongoClient = new MongoClient(process.env.MONGO_URI as string);
-    this.collection = this.mongoClient.db('task-tracker').collection('tasks');
-  }
+	/**
+	 * The profile used for authentication.
+	 */
+	public readonly profile: ILogin;
 
-  public get dbCollection(): Collection {
-    return this.collection;
-  }
+	/**
+	 * Creates a new instance of the Mongo class.
+	 * @param _profile - The profile used for authentication.
+	 */
+	public constructor(_profile: ILogin) {
+		this.client = new MongoClient(process.env.MONGO_URI as string);
+		this.database = this.client.db('task-tracker');
+		this.collection = this.database.collection('tasks');
+
+		this.profile = _profile;
+	}
+
+	/**
+	 * Returns the MongoDB collection used for database operations.
+	 */
+	public get dbCollection(): Collection {
+		return this.collection;
+	}
+
+	/**
+	 * Finds the profile in the database.
+	 * @returns A promise that resolves when the profile is found.
+	 */
+	public async doesProfileExist(): Promise<boolean> {
+		const profile = await this.collection.findOne({
+			...this.profile
+		});
+
+		return !!profile;
+	}
+
+	/**
+	 * Adds the profile to the database.
+	 * @returns A promise that resolves when the profile is added.
+	 */
+	public async addProfile(): Promise<void> {
+		const profile: IProfile = {
+			...this.profile,
+			tasks: []
+		};
+
+		await this.collection.insertOne(profile);
+	}
 }
