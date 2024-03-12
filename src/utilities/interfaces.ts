08c82ac95c967b7ec2d8bfbbe19499aa137bfcdc
Yurem <yuremtovar1@gmail.com>
2024-03-12 15:15:48 -0500

    Add interfaces for login, tasks, and user profile

---
+++ src/utilities/interfaces.ts
@@ -0,0 +1,28 @@
+/**
+ * @interface
+ * Represents the login information to be used for authentication.
+ */
+export interface ILogin {
+  username: string;
+  password: string;
+}
+
+/**
+ * @interface
+ * Represents the tasks data for the user profile.
+ */
+export interface ITasks {
+  dataCreated: string;
+  name: string;
+  value: string;
+}
+
+/**
+ * @interface
+ * Represents the user profile information
+ */
+export interface IProfile {
+  username: string;
+  password: string;
+  tasks: ITasks[];
+}
