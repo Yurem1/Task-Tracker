e47f6485b7c390ac094c77454dbb3db29cfa0573
Yurem <yuremtovar1@gmail.com>
2024-03-11 22:00:18 -0500

    Update sign-in API route

--- src/app/auth/api/route.ts
+++ src/app/auth/api/route.ts
@@ -1,15 +1,32 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
+import { Mongo } from '@/app/auth/api/mongo';
 
+/**
+ * Handles the POST request for the sign-in page.
+ * @param req The incoming request.
+ * @returns The response.
+ */
 export async function POST(req: NextRequest): Promise<NextResponse> {
 	const request = await req.json();
+
+  const mongo = new Mongo().dbCollection;
+  console.log(await mongo.countDocuments())
+  await mongo.insertOne(request);
 
 	return new NextResponse(JSON.stringify(request), {
 		headers: {
 			"content-type": "application/json",
 		},
+    status: 200,
+    statusText: 'OK',
 	});
 }
 
+/**
+ * Handles the GET request for the sign-in page.
+ * @param req The incoming request.
+ * @returns The response.
+ */
 export async function GET(req: NextRequest): Promise<NextResponse> {
 	return new NextResponse('Hello World');
 }
