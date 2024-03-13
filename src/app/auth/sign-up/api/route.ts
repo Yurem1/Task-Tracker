5968db59571591fc044c45bc143ed7a26a4f68e1
Yurem <yuremtovar1@gmail.com>
2024-03-12 19:19:58 -0500

    Refactor & add comments, unhardcode status properties

--- src/app/auth/sign-up/api/route.ts
+++ src/app/auth/sign-up/api/route.ts
@@ -1,31 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { HTTPConstants } from '@/utilities/constants';
 import { Mongo } from '@/app/auth/api/mongo';
 
 /**
- * Handles the POST request for the sign-in page.
- * @param res The incoming request.
- * @returns The response.
+ * Handles the POST request for signing up a user.
+ * 
+ * @param req - The NextRequest object representing the incoming request.
+ * @returns A Promise that resolves to a NextResponse object representing the response to the request.
  */
 export async function POST(req: NextRequest): Promise<NextResponse> {
+
+  // Parse the request body as JSON
   const res = await req.json();
 
+  // Create a new instance of the Mongo class with the parsed JSON data
   const client = new Mongo(res);
 
+  // Checks whether the account exists, returns false if not
   if(await client.doesProfileExist()) {
+    // Finally returns the response with status code 409
     return new NextResponse(JSON.stringify(res), {
       headers: {
         'Content-Type': 'application/json',
       },
-      status: 409,
-      statusText: 'Account already exists',
+      ...HTTPConstants.ACCOUNT_ALREADY_EXISTS
     });
   }
 
-	return new NextResponse(JSON.stringify(res), {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-    status: 200,
-    statusText: 'OK',
-	});
+  // Add the profile to the database
+  await client.addProfile();
+
+  // Fetch the created profile from the database
+  const profile = await client.fetchProfile();
+  
+  return new NextResponse(JSON.stringify(profile), {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    ...HTTPConstants.ACCOUNT_CREATED_SUCCESSFULLY
+  });
 }
