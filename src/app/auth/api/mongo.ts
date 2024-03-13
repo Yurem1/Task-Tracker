e8ba2624a3e571d11981cc9cbd4701b0d237e16a
Yurem <yuremtovar1@gmail.com>
2024-03-12 22:28:57 -0500

    convert indentation to spaces

--- src/app/auth/api/mongo.ts
+++ src/app/auth/api/mongo.ts
@@ -6,72 +6,72 @@ import { MongoClient, Collection, Db } from 'mongodb';
  * Represents a MongoDB connection and provides methods to interact with the database.
  */
 export class Mongo {
-	private client: MongoClient;
-	private database: Db;
-	private collection: Collection;
+  private client: MongoClient;
+  private database: Db;
+  private collection: Collection;
 
-	/**
-	 * The profile used for authentication.
-	 */
-	public readonly profile: ILogin;
+  /**
+   * The profile used for authentication.
+   */
+  public readonly profile: ILogin;
 
-	/**
-	 * Creates a new instance of the Mongo class.
-	 * @param _profile - The profile used for authentication.
-	 */
-	public constructor(_profile: ILogin) {
-		this.client = new MongoClient(process.env.MONGO_URI as string);
-		this.database = this.client.db('task-tracker');
-		this.collection = this.database.collection('tasks');
+  /**
+   * Creates a new instance of the Mongo class.
+   * @param _profile - The profile used for authentication.
+   */
+  public constructor(_profile: ILogin) {
+    this.client = new MongoClient(process.env.MONGO_URI as string);
+    this.database = this.client.db('task-tracker');
+    this.collection = this.database.collection('tasks');
 
-		this.profile = _profile;
-	}
+    this.profile = _profile;
+  }
 
-	/**
-	 * Returns the MongoDB collection used for database operations.
-	 */
-	public get dbCollection(): Collection {
-		return this.collection;
-	}
+  /**
+   * Returns the MongoDB collection used for database operations.
+   */
+  public get dbCollection(): Collection {
+    return this.collection;
+  }
 
-	/**
-	 * Finds the profile in the database.
-	 * @returns A boolean that resolves when the profile is found.
-	 */
-	public async doesProfileExist(): Promise<boolean> {
-		const profile = await this.collection.findOne({
-			...this.profile
-		});
+  /**
+   * Finds the profile in the database.
+   * @returns A boolean that resolves when the profile is found.
+   */
+  public async doesProfileExist(): Promise<boolean> {
+    const profile = await this.collection.findOne({
+      ...this.profile
+    });
 
-		return !!profile;
-	}
+    return !!profile;
+  }
 
-	/**
-	 * Adds the profile to the database.
-	 * @returns A promise that resolves when the profile is added.
-	 */
-	public async addProfile(): Promise<void> {
-		const profile: IProfile = {
-			...this.profile,
-			tasks: []
-		};
+  /**
+   * Adds the profile to the database.
+   * @returns A promise that resolves when the profile is added.
+   */
+  public async addProfile(): Promise<void> {
+    const profile: IProfile = {
+      ...this.profile,
+      tasks: []
+    };
 
-		if(await this.doesProfileExist()) {
-			return;
-		}
+    if(await this.doesProfileExist()) {
+      return;
+    }
 
-		await this.collection.insertOne(profile);	
-	}
-	
-	/**
-	 * Fetches the user profile from the database.
-	 * @returns A Promise that resolves to the fetched profile or null if not found.
-	 */
-	public async fetchProfile(): Promise<IProfile | null> {
-		const req = await this.collection.findOne<IProfile>({
-			...this.profile
-		});
-		
-		return req;
-	}
+    await this.collection.insertOne(profile);	
+  }
+  
+  /**
+   * Fetches the user profile from the database.
+   * @returns A Promise that resolves to the fetched profile or null if not found.
+   */
+  public async fetchProfile(): Promise<IProfile | null> {
+    const req = await this.collection.findOne<IProfile>({
+      ...this.profile
+    });
+    
+    return req;
+  }
 }
--- src/app/auth/api/route.ts
+++ src/app/auth/api/route.ts
@@ -7,34 +7,25 @@ import { Mongo } from '@/app/auth/api/mongo';
  * @returns The response.
  */
 export async function POST(req: NextRequest): Promise<NextResponse> {
-	const res = await req.json();
+  const res = await req.json();
 
-	const mongo = new Mongo(res);
+  const mongo = new Mongo(res);
 
-	if(await mongo.doesProfileExist()) {
-		return new NextResponse(JSON.stringify(res), {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			status: 409,
-			statusText: 'Account already exists',
-		});
-	}
+  if(await mongo.doesProfileExist()) {
+    return new NextResponse(JSON.stringify(res), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      status: 409,
+      statusText: 'Account already exists',
+    });
+  }
 
-	return new NextResponse(JSON.stringify(res), {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		status: 200,
-		statusText: 'OK',
-	});
-}
-
-/**
- * Handles the GET request for the sign-in page.
- * @param req The incoming request.
- * @returns The response.
- */
-export async function GET(req: NextRequest): Promise<NextResponse> {
-	return new NextResponse('Hello World');
+  return new NextResponse(JSON.stringify(res), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    status: 200,
+    statusText: 'OK',
+  });
 }
