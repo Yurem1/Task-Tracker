9b5b8209d44386f8d7c87228990a99dcb7206f45
Yurem <yuremtovar1@gmail.com>
2024-03-13 20:11:22 -0500

    Add profile fetching logic to dashboard tasks page

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -1,9 +1,34 @@
+'use client';
+
+import { ProfileConstants } from '@/utilities/constants';
+import { IProfile } from '@/utilities/interfaces';
 import React from 'react';
 
 export default function Page(): React.JSX.Element {
+  const [profile, setProfile] = React.useState<IProfile>();
+
+  React.useEffect(() => {
+    const fetchProfile = async () => {
+      const request = await fetch('/dashboard/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(null)
+      });
+
+      if(request.ok) {
+        const response: IProfile = await request.json();
+        setProfile(() => response);
+      }
+    }
+
+    fetchProfile();
+  }, []);
+
   return (
-    <div className='h-full'>
-      Hello
+    <div className='flex flex-col items-center h-full'>
+      
     </div>
   )
 }
