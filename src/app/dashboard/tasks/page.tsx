a1e5d79920230f7987c4d1d2316f11a2b189062a
Yurem <yuremtovar1@gmail.com>
2024-03-14 12:47:28 -0500

    Add task button functionality

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -3,13 +3,15 @@
 import { ProfileConstants } from '@/utilities/constants';
 import { IProfile } from '@/utilities/interfaces';
 import React from 'react';
+import AddTasks from './components/addTasks';
 
 export default function Page(): React.JSX.Element {
   const [profile, setProfile] = React.useState<IProfile>();
+  const [addTask, setTask] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     const fetchProfile = async () => {
-      const request = await fetch('/dashboard/api', {
+      const req = await fetch('/dashboard/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -17,18 +19,31 @@ export default function Page(): React.JSX.Element {
         body: JSON.stringify(null)
       });
 
-      if(request.ok) {
-        const response: IProfile = await request.json();
-        setProfile(() => response);
+      if(req.ok) {
+        const res: IProfile = await req.json();
+        setProfile(() => res);
       }
     }
 
     fetchProfile();
   }, []);
 
+  const buttonClick = () => {
+    setTask(task => !task)
+  }
+
   return (
     <div className='flex flex-col items-center h-full'>
-      
+      <div className='m-5'>
+        <button type='button' onClick={buttonClick}
+          className='bg-gray-200 rounded-lg px-4 border-2 border-black 
+           hover:scale-95 active:scale-90'>
+            Add a task
+        </button>
+      </div>
+      {addTask && (
+        <AddTasks/>
+      )}
     </div>
   )
 }
