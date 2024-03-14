c020f4670d12ec620e3a9aa8d605302ebd4ee57e
Yurem <yuremtovar1@gmail.com>
2024-03-13 20:11:09 -0500

    Refactor POST function in route.ts to include profile and return success response

--- src/app/dashboard/api/route.ts
+++ src/app/dashboard/api/route.ts
@@ -1,8 +1,17 @@
 import { IProfile } from "@/utilities/interfaces";
 import { NextRequest, NextResponse } from "next/server";
+import { HTTPConstants } from "@/utilities/constants";
+
+let profile: IProfile | undefined;
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const res = await req.json();
-
-  return new NextResponse(JSON.stringify(req))
+  profile = !!res ? res : profile;
+  
+  return new NextResponse(JSON.stringify(profile), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...HTTPConstants.ACCOUNT_POSTED_SUCCESSFULLY,
+  });
 }
