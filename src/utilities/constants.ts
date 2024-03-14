3807c103045d76cd2ba43cef9df946294a2a0239
Yurem <yuremtovar1@gmail.com>
2024-03-14 12:57:55 -0500

    Add ITasks interface and TaskConstants to constants.ts

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,4 +1,4 @@
-import { IHTTPConstants, ILogin, IProfile } from '@/utilities/interfaces';
+import { IHTTPConstants, ILogin, IProfile, ITasks } from '@/utilities/interfaces';
 import { ObjectId } from 'mongodb';
 
 /**
@@ -14,7 +14,6 @@ export const FormConstants = {
     username: '',
     password: ''
   }
-
 } satisfies Record<string, ILogin>;
 
 /**
@@ -29,30 +28,60 @@ export const ProfileConstants = {
     username: '',
     password: '',
     tasks: [],
-  }
+  },
 } satisfies Record<string, IProfile>;
+
+/**
+ * @constant
+ * Represents the task constants
+ */
+export const TaskConstants = {
+  /**
+   * The default task object.
+   */
+  DEFAULT_TASK: {
+    name: '',
+    value: '',
+    dataCreated: new Date()
+  }
+} satisfies Record<string, ITasks>
 
 /**
  * @constant
  * HTTP Constants object that contains predefined status codes and status texts.
  */
 export const HTTPConstants = {
+  /**
+   * Account already exists constant.
+   */
   ACCOUNT_ALREADY_EXISTS: {
     status: 409,
     statusText: 'Account already exists!'
   },
+  /**
+   * Account not found constant.
+   */
   ACCOUNT_NOT_FOUND: {
     status: 404,
     statusText: 'Account not found.'
   },
+  /**
+   * Account created successfully constant.
+   */
   ACCOUNT_CREATED_SUCCESSFULLY: {
     status: 200,
     statusText: 'Account created successfully.'
   },
+  /**
+   * Account found successfully constant.
+   */
   ACCOUNT_FOUND_SUCCESSFULLY: {
     status: 200,
     statusText: 'Account found successfully.'
   },
+  /**
+   * Account posted successfully constant.
+   */
   ACCOUNT_POSTED_SUCCESSFULLY: {
     status: 200,
     statusText: 'Account posted successfully.'
