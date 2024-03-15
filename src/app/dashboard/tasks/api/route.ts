e09275dde28d41f73f5a0a678b8d25a250ef235e
Yurem <yuremtovar1@gmail.com>
2024-03-15 12:36:28 -0500

    Add functionality to add new tasks

--- src/app/dashboard/tasks/api/route.ts
+++ src/app/dashboard/tasks/api/route.ts
@@ -1,11 +1,22 @@
+import { Mongo } from "@/app/auth/api/mongo";
+import { HTTPConstants } from "@/utilities/constants";
+import { IDashboardTasks } from "@/utilities/interfaces";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  const res = await req.json();
-  
-  return new NextResponse(JSON.stringify(res), {
+  const res: IDashboardTasks = await req.json();
+
+  const client = new Mongo({
+    username: res.profile.username,
+    password: res.profile.password
+  });
+
+  const newTasks = await client.addTask(res.newTask);
+
+  return new NextResponse(JSON.stringify(newTasks), {
     headers: {
       'Content-Type': 'application/json'
-    }
-  })
+    },
+    ...HTTPConstants.ADDED_TASK_SUCCESSFULLY
+  });
 }
